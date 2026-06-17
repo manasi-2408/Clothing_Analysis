c31bbe660479a5228363cc74b0780d2adfa12a4a
Manasi Bhutra <manasibhutra24@gmail.com>
2026-06-17 20:05:22 +0530

    Added sql docs

---
+++ SQL/clothing.sql
@@ -0,0 +1,10 @@
+-- Problem Statement
+-- Retail businesses often struggle to understand customer purchasing behaviour and 
+-- identify hidden relationships among products. Without analyzing transaction data, 
+-- businesses cannot effectively segment customers, recommend products, or implement 
+-- cross-selling strategies. Therefore, this project aims to analyze customer purchase 
+-- transactions to understand purchasing behaviour, discover frequently purchased product 
+-- combinations, identify customer segments, and generate actionable business recommendations.
+
+-- Business Questions to be asked
+-- Q1. 
